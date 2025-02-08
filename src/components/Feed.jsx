8c328852b240d687d0740bd684e27389e33e9415
Nadeem Shaik <nadeem.shaik227@gmail.com>
2025-02-08 17:36:31 +0530

    feed edit changes

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,13 +2,11 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../store/Slices/feedSlice";
-import { useState } from "react";
 import { useEffect } from "react";
 import UserCard from "./UserCard";
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
-  const [error, setError] = useState();
   const dispatch = useDispatch();
   const fetchFeedData = async () => {
     if (feed) return;
@@ -16,20 +14,23 @@ const Feed = () => {
       const res = await axios.get(BASE_URL + "/feed", {
         withCredentials: true,
       });
-      dispatch(addFeed(res?.data));
+      dispatch(addFeed(res?.data?.data));
     } catch (error) {
-      setError(error.response.data);
+      console.log(error);
     }
   };
   useEffect(() => {
     fetchFeedData();
   });
 
+  if (!feed) return;
+  if (feed.length <= 0)
+    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
+
   return (
-    feed?.data.length && (
+    feed && (
       <div className="flex justify-center my-10">
-        <p>{error}</p>
-        <UserCard user={feed.data[0]} isFeedoptions={true} />
+        <UserCard user={feed[0]} isFeedoptions={true} />
       </div>
     )
   );
